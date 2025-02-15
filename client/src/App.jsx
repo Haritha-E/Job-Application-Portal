76c3e9b8b076580c355ded7c88f4fead8ba4e656
Haritha-E <harithaeswaran@gmail.com>
2025-02-15 11:22:32 +0530

    Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import PostVacancy from "./pages/PostVacancy";
+import ExploreJobs from "./pages/ExploreJobs"; 
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/post-vacancies" element={<PostVacancy />} />
+          <Route path="/jobs" element={<ExploreJobs />} />
+
+        </Routes>
+      </div>
     </Router>
   );
 }
